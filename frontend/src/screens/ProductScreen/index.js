43fa61b4125d615366e230f0738536d173e4df6b
CoderBoy4715 <alijahlemay12@gmail.com>
2020-10-03 12:52:44 -0700

    Stuck at a bug

--- frontend/src/screens/ProductScreen/index.js
+++ frontend/src/screens/ProductScreen/index.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+} from 'react-router-bootstrap';
+import Rating from '../../components/Rating';
+import products from '../../products';
 
-const Product = () => {
-  return <div></div>;
+const Product = ({ match }) => {
+  const product = products.find((p) => p._id === match.params.id);
+  return (
+    <Fragment>
+      <Link className='btn btn-light my-3' to='/' />
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h2>{product.name}</h2>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Fragment>
+  );
 };
 
 export default Product;
