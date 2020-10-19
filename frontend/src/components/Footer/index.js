7f5df96a98bf8321358bc37f3e7c2773d5aea203
CoderBoy4715 <alijahlemay12@gmail.com>
2020-10-19 08:44:47 -0700

    changes to footer

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -6,7 +6,12 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>copyright &copy; Proshop</Col>
+          <Col className='text-center py-3'>
+            Copyright &copy; 2020{' '}
+            <a target='_blank' href='//www.thewebdev.net'>
+              LemaTech LLC
+            </a>
+          </Col>
         </Row>
       </Container>
     </footer>
